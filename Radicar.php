79425bc23ab946dcc311d2858c68f05d398b1757
EDWIN YAMITH OLAYA MANTILLA <eyolaya32@misena.edu.co>
2019-11-14 08:39:29 -0500

    Modificado title Radidado.php

--- Radicar.php
+++ Radicar.php
@@ -9,7 +9,7 @@
 <!DOCTYPE html>
 <html lang="es">
   <head>
-    <title>Radicar</title>
+    <title>Radicar Prueba V2.1</title>
     <meta name="viewport" content="initial-scale=1.0">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -114,11 +114,7 @@
     </form>
     </div>
 
-
-
-
-
-	<footer>
+<footer>
 		<div class="container">
 		<br>
 		    <p>Derechos reservados de autor Edwin Olaya Mantilla, Versi&oacute;n 2.1 email: monomanti@gmail.com  Movil: 315 881 96 51</p>
@@ -132,13 +128,8 @@
     <script src="../Bootstrap/popper/popper.min.js" ></script>
     <script src="../Bootstrap/js/bootstrap.min.js" ></script>
     <script src="../Bootstrap/js/jquery.easy-autocomplete.min.js" ></script>
-    <!--
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-    <!-->
-    
-    <script>
+
+  <script>
 		//creacion de script para autocompletar
 		var options = {
 
